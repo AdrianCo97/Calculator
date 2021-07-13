36e85963dfd7245599fe7620346668c026509401
AdrianCo97 <adrian.corazza@outlook.com>
2021-07-13 16:06:49 +0200

    Made a component called NumberCalculator that will be used for additions.

---
+++ src/components/NumberCalculator.js
@@ -0,0 +1,18 @@
+import React from "react";
+class NumberCalculator extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+     
+    };
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default NumberCalculator;
