5f3457a2b37a407fbd97f1e45c1dfa3cd57b6be9
AdrianCo97 <adrian.corazza@outlook.com>
2021-07-15 13:58:36 +0200

    Added the multiplicationComponent into the condition function.

--- src/components/NumberCalculator.js
+++ src/components/NumberCalculator.js
@@ -1,11 +1,11 @@
 import React from "react";
 import AdditionComponent from "./AdditionComponent";
+import MultiplicationComponent from "./MultiplicationComponent";
 class NumberCalculator extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      sum: 0,
       type: "addition",
     };
   }
@@ -14,14 +14,11 @@ class NumberCalculator extends React.Component {
     const typeHandler = () => {
       if (this.state.type === "addition") {
         return <AdditionComponent />;
+      } else if (this.state.type === "multiplication") {
+        return <MultiplicationComponent />;
       }
     };
-    return (
-      <div id="content">
-        {typeHandler()}
-        <h2>The result is: {this.state.sum}</h2>
-      </div>
-    );
+    return <div id="content">{typeHandler()}</div>;
   }
 }
 
