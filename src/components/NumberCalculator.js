8ae03053131127f7440ece7420fdb95ea5d1dc1f
AdrianCo97 <adrian.corazza@outlook.com>
2021-07-15 11:48:25 +0200

    Made a typeHandler function that is used for conditional rendering

--- src/components/NumberCalculator.js
+++ src/components/NumberCalculator.js
@@ -1,16 +1,27 @@
 import React from "react";
+import AdditionComponent from "./AdditionComponent";
 class NumberCalculator extends React.Component {
   constructor() {
     super();
 
     this.state = {
       sum: 0,
-      type: "",
+      type: "addition",
     };
   }
 
   render() {
-    return <h2>The result is: {this.state.sum}</h2>;
+    const typeHandler = () => {
+      if (this.state.type === "addition") {
+        return <AdditionComponent />;
+      }
+    };
+    return (
+      <div id="content">
+        {typeHandler()}
+        <h2>The result is: {this.state.sum}</h2>
+      </div>
+    );
   }
 }
 
