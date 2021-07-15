ac8cf08e331ab66758e10fbe0b189543b6bff488
AdrianCo97 <adrian.corazza@outlook.com>
2021-07-15 11:50:39 +0200

    Fixed the component so that it can actually run.

--- src/components/AdditionComponent.js
+++ src/components/AdditionComponent.js
@@ -1,33 +1,35 @@
 import numberCalculatorStyle from "./numberCalculatorStyle.module.css";
 function AdditionComponent() {
-  additionFunction = (event) => {
+  function additionFunction(event) {
     event.preventDefault();
 
     let x = +document.getElementById("x-input").value;
     let y = +document.getElementById("y-input").value;
 
     let result = x + y;
-  };
+  }
 
-  <div className={numberCalculatorStyle.divStyle}>
-    <form
-      className={numberCalculatorStyle.formStyle}
-      onSubmit={this.additionFunction}
-    >
-      <input
-        className={numberCalculatorStyle.inputStyle}
-        id="x-input"
-        type="text"
-      ></input>
-      <h3>+</h3>
-      <input
-        className={numberCalculatorStyle.inputStyle}
-        id="y-input"
-        type="text"
-      ></input>
-      <button>Calculate!</button>
-    </form>
-  </div>;
+  return (
+    <div className={numberCalculatorStyle.divStyle}>
+      <form
+        className={numberCalculatorStyle.formStyle}
+        onSubmit={additionFunction}
+      >
+        <input
+          className={numberCalculatorStyle.inputStyle}
+          id="x-input"
+          type="text"
+        ></input>
+        <h3>+</h3>
+        <input
+          className={numberCalculatorStyle.inputStyle}
+          id="y-input"
+          type="text"
+        ></input>
+        <button>Calculate!</button>
+      </form>
+    </div>
+  );
 }
 
 export default AdditionComponent;
