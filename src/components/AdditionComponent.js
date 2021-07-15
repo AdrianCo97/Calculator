fcc9178e096725c35ec3c4fdbd3ccc6eed929457
AdrianCo97 <adrian.corazza@outlook.com>
2021-07-15 13:56:27 +0200

    Changed the component into a class component instead.

--- src/components/AdditionComponent.js
+++ src/components/AdditionComponent.js
@@ -1,35 +1,50 @@
+import React from "react";
+
 import numberCalculatorStyle from "./numberCalculatorStyle.module.css";
-function AdditionComponent() {
-  function additionFunction(event) {
+class AdditionComponent extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sum: 0,
+    };
+  }
+  additionFunction = (event) => {
     event.preventDefault();
 
     let x = +document.getElementById("x-input").value;
     let y = +document.getElementById("y-input").value;
 
     let result = x + y;
-  }
 
-  return (
-    <div className={numberCalculatorStyle.divStyle}>
-      <form
-        className={numberCalculatorStyle.formStyle}
-        onSubmit={additionFunction}
-      >
-        <input
-          className={numberCalculatorStyle.inputStyle}
-          id="x-input"
-          type="text"
-        ></input>
-        <h3>+</h3>
-        <input
-          className={numberCalculatorStyle.inputStyle}
-          id="y-input"
-          type="text"
-        ></input>
-        <button>Calculate!</button>
-      </form>
-    </div>
-  );
+    this.setState({ sum: result });
+  };
+
+  render() {
+    return (
+      <div className={numberCalculatorStyle.divStyle}>
+        <h2>Addition</h2>
+        <form
+          className={numberCalculatorStyle.formStyle}
+          onSubmit={this.additionFunction}
+        >
+          <input
+            className={numberCalculatorStyle.inputStyle}
+            id="x-input"
+            type="text"
+          ></input>
+          <h3>+</h3>
+          <input
+            className={numberCalculatorStyle.inputStyle}
+            id="y-input"
+            type="text"
+          ></input>
+          <button>Calculate!</button>
+        </form>
+        <h3>sum is: {this.state.sum}</h3>
+      </div>
+    );
+  }
 }
 
 export default AdditionComponent;
